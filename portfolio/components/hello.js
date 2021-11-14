440447a58db8c751380a01dabf9f5ba948f5af2c
Kerstin Reichinger <kr@cognify.ai>
2021-11-14 18:36:48 +0100

    Add box-shadow and center aboutme

--- portfolio/components/hello.js
+++ portfolio/components/hello.js
@@ -10,7 +10,7 @@ export default function Hello() {
             p={"50px"}
             >
                 <GridItem rowSpan={1} colSpan={5}><Heading  size="3xl"  bgGradient="linear(to-l, yellow.400, orange.500, pink.400)" bgClip="text">Hi, I&#39;m Kerstin...</Heading></GridItem>
-                <GridItem colSpan={1} ><Avatar boxSize="200px" name="Segun Adebayo" src="images/me.JPG" /></GridItem>
+                <GridItem colSpan={1} ><Avatar boxSize="200px" name="Segun Adebayo" src="images/me.JPG" boxShadow="2px 4px 15px 0px #000000"/></GridItem>
                 <GridItem colSpan={4} margin="auto"><Text fontSize="xl">...and I am a web developer based in Austria with a passion for learning new stuff, hiking in the beautiful mountains, listen to loud music and push me to my personal limits in the gym. Currently studying Multimedia Technology at the University of Applied Sciences in Salzburg. </Text></GridItem>
             </Grid>
         </section> 
