6b2da2dbc7602718889b7d2b312810ff96d8a27c
Kerstin Reichinger <kr@cognify.ai>
2021-11-16 15:15:46 +0100

    Make projcts responsibe

--- portfolio/components/projects.js
+++ portfolio/components/projects.js
@@ -4,9 +4,24 @@ import Project from './project';
 export default function Projects() {
     return (
         <section id="projects">
-            <Center><Heading pr={"50px"} pl={"50px"} lineHeight={1.5} bgGradient="linear(to-l, yellow.400, orange.500, pink.400)" bgClip="text" size="2xl">Projects</Heading></Center>
-             <Flex  pr={"50px"} pl={"50px"} flexWrap="wrap" justifyContent="center" flexDir="row" spacing="24px">
-                 <Project 
+            <Center>
+                <Heading 
+                    lineHeight={1.5} 
+                    bgGradient="linear(to-l, yellow.400, orange.500, pink.400)" 
+                    bgClip="text" 
+                    size="2xl"
+                >
+                    Projects
+                </Heading>
+            </Center>
+
+             <Flex  
+                flexWrap="wrap" 
+                justifyContent="center" 
+                flexDir="row" 
+                spacing="20px"
+            >
+                <Project 
                     title="DogDate" 
                     description="DogDate is my first multimedia project at university. It's a platform where dog owners can search for dogfriends (social media for dogs). Once the profile has been created, you can search for dog friends on the map. Either you contact the dog owner directly via the message function or you look at the profile." 
                     link="https://portfolio.fh-salzburg.ac.at/projects/2021-dog-date"
